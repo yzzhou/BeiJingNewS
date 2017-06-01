6a06543085bdad7ff7f778607b0fe261fb44fe86
yzzhou <920115128@qq.com>
2017-06-01 19:07:57 +0800

    Initial commit

---
+++ app/src/main/java/myapplication/beijingnews/activity/WelcomeActivity.java
@@ -0,0 +1,60 @@
+package myapplication.beijingnews.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.widget.RelativeLayout;
+
+import myapplication.beijingnews.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+    private RelativeLayout activity_main;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        activity_main = (RelativeLayout)findViewById(R.id.activity_main);
+        RotateAnimation ra = new RotateAnimation(0,360,RotateAnimation.RELATIVE_TO_SELF,0.5f,RotateAnimation.RELATIVE_TO_SELF,0.5f);
+        ra.setFillAfter(true);
+        ra.setDuration(2000);
+
+        ScaleAnimation sa = new ScaleAnimation(0,1,0,1,RotateAnimation.RELATIVE_TO_SELF,0.5f,RotateAnimation.RELATIVE_TO_SELF,0.5f);
+        sa.setDuration(2000);
+        sa.setFillAfter(true);
+
+        AlphaAnimation aa = new AlphaAnimation(0,1);
+        aa.setFillAfter(true);
+        aa.setDuration(2000);
+
+        AnimationSet set =new AnimationSet(false);
+        set.addAnimation(ra);
+        set.addAnimation(aa);
+        set.addAnimation(sa);
+
+        activity_main.startAnimation(set);
+
+        set.setAnimationListener(new MyAnimationListener());
+    }
+
+    private class MyAnimationListener implements Animation.AnimationListener {
+        @Override
+        public void onAnimationStart(Animation animation) {
+
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
+    }
+}
